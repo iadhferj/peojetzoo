8711131bc1626e7311ef22350f000d548f32800a
ferjaoui iadh <iadh.ferjaoui@esprit.tn>
2023-10-30 15:35:51 +0000

    test

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+       /* Zoo z1 =new Zoo(2,"ariana");
+        Zoo z2 =new Zoo(20,"fouchena");
+        System.out.println(z1.isComplete(4));
+        */
+        /*
+Scanner in = new Scanner(System.in);
+        System.out.println(".........");
+*/
+
+
+    }
+}
---
+++ src/Zoo.java
@@ -0,0 +1,16 @@
+/*public class Zoo {
+    int nbrCages;
+    String location;
+
+    public Zoo(int nbrC, String location){
+        this.nbrCages=nbrC;
+        this.location = location;
+
+
+    }
+
+    public boolean isComplete (int nbr){
+        return nbrCages > nbr;
+    }
+
+}*/
